6722e08222068ccbea723b6d9559143c89533bc2
adamn102 <anitido@g.harvard.edu>
2020-10-13 17:25:44 -0400

    created setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+from distutils.core import setup
+
+setup(name='yUMItools',
+      description='UMI tools for ERR-Seq Project',
+      author_email=anitido@g.harvard.edu,
+      url='https://github.com/adamn102/yUMItools/',
+      packages=[’yUMItools’])
