49438a0e103dce5eddca93add873af977938a01b
adamn102 <anitido@g.harvard.edu>
2021-11-03 11:29:40 -0400

    data simulation reverse transcription test

--- yUMItools/tests/test_datasim.py
+++ yUMItools/tests/test_datasim.py
@@ -62,3 +62,38 @@ def test_generate_barcode_library():
     assert hamming(s.barcode_library_list[0], s.barcode_library_list[1]) > 0
 
     del s
+
+
+def test_reverse_transcribe_library():
+    # filepath
+    test_data_fasta_filepath = 'test_data/reference_sequence/Rp0-reference.fa'
+
+    # create sequence object and load fasta file
+    s = TestTube(test_data_fasta_filepath)
+
+    library_sequence = s.reference_sequence_dict['Rp0-reference']
+    s.find_barcodes(library_sequence=library_sequence)
+
+    # only generate one clone - this allows us to easily count mutations
+    s.generate_barcode_library(clones=1)
+
+    # test with no mutations
+    rt_library = reverse_transcribe_library(s.barcode_library_list,
+                                            clones=2,
+                                            mut_type='random',
+                                            mutation_rate=0)
+    assert hamming(rt_library[0], rt_library[1]) == 0
+
+    # test with low mutation rate
+    rt_library = reverse_transcribe_library(s.barcode_library_list,
+                                            clones=2,
+                                            mut_type='random',
+                                            mutation_rate=1. / 10000)
+    assert hamming(rt_library[0], rt_library[1]) <= 10
+
+    # test with high mutation rate
+    rt_library = reverse_transcribe_library(s.barcode_library_list,
+                                            clones=2,
+                                            mut_type='random',
+                                            mutation_rate=1. / 100)
+    assert hamming(rt_library[0], rt_library[1]) >= 10
